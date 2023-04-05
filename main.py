772442b1dfea0314811f93039f4b951d2d1751bc
Jewel John <jeweljohnl0v3@gmail.com>
2023-04-05 21:47:55 +0530

    QtWidgets Window

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import cv2
-image = cv2.imread("Image.jpg")
-grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-invert = cv2.bitwise_not(grey_img)
-blur = cv2.GaussianBlur(invert, (21,21), 0)
-inverted_blur = cv2.bitwise_not(blur)
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
-sketch = cv2.divide(grey_img, inverted_blur, scale=256.0)
 
-cv2.imwrite("Sketch.png", sketch)
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(500, 300, 700, 500)
+    win.setWindowTitle("ArtMachine")
+    win.setWindowIcon(QIcon("Icon.png"))
+    win.show()
+    sys.exit(app.exec_())
+
+window()
